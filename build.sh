20643256fb9358a0f71e65daaa7d437e7e7c5e37
Akshit Garg <garg.akshit@gmail.com>
2020-06-29 17:02:07 +0530

    [Build] Prepare for release

--- build.sh
+++ build.sh
@@ -1,3 +1,5 @@
 echo "Building for android..."
-flutter build apk --release --split-per-abi
+flutter build apk --target-platform android-arm --split-per-abi
+flutter build apk --target-platform android-x64 --split-per-abi
+flutter build apk --target-platform android-arm64 --split-per-abi
 echo "Done"
